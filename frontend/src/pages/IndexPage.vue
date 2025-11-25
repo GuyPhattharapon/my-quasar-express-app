5acfa272c72e04784fab45da4885493feb0e9390
Phattharapon Kitanan <p0932150480@gmail.com>
2025-11-25 14:28:24 +0700

    แก้ไข IndexPage.vue

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,13 +1,10 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div class="q-pa-lg">
+    <q-option-group
+      v-model="group"
+      :options="options"
+      color="green"
+      type="checkbox"
+    />
+  </div>
 </template>
-
-<script setup>
-//
-</script>
