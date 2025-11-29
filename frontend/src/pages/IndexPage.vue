185f4b1810aa4419b3fea77114707cd03d365c6a
Phattharapon Kitanan <p0932150480@gmail.com>
2025-11-29 19:37:12 +0700

    Add a map

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="q-pa-lg">
-    <q-option-group
-      v-model="group"
-      :options="options"
-      color="green"
-      type="checkbox"
-    />
-  </div>
+  <q-page class="q-pa-none">
+    <div class="map-wrapper">
+      <iframe
+        src="/longan_map.html"
+        class="map-iframe"
+        frameborder="0"
+      ></iframe>
+    </div>
+  </q-page>
 </template>
+
+<style>
+.map-wrapper {
+  width: 100%;
+  height: calc(100vh - 50px); /* ลดสูงลงไม่ให้กิน header */
+}
+
+.map-iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
